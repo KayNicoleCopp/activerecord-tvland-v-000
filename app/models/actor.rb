e713447e4459024e329d001588c7e0050ff15c2b
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2019-04-05 15:13:36 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,6 @@
 class Actor < ActiveRecord::Base
-  has_many :shows
-  has_many :shows, through: :characters
+  has_many :show
+  has_many :show, through: :character
 
 def full_name
   "#{self.first_name} #{self.last_name}"
